26620ae7c4e0c18ab7375e878e7cee411be03ad4
uni-SanjayS <sanjay.s@uniphore.com>
2024-05-10 11:55:35 +0530

    Added tag v1.0.0

--- puppy.go
+++ puppy.go
@@ -1,9 +1,22 @@
 package puppy
 
+import "fmt"
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! woof! woof! Woof!"
+}
+
+func BigBark() string {
+	return WhenGrowsup(Bark())
+}
+
+func BigBarks() string {
+	return WhenGrowsup(Barks())
+}
+func Fromv1() {
+	fmt.Println("V!.0.0")
 }
